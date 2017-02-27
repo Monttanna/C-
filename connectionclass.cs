11b8a6ec03bfd626f3ea48c7d594f9f281c34b35
Erith Monttanna <erith.armentta@gmail.com>
2017-02-27 12:39:40 -0700

    Update connectionclass.cs

--- connectionclass.cs
+++ connectionclass.cs
@@ -8,35 +8,27 @@ using System.Data;
 
 namespace Appname // Here you have to put the application name.
 {
-    class common
-    {
+    class common {
         SqlConnection con = new SqlConnection("Data Source=PC\\SQLEXPRESS;Initial Catalog=databasename;Trusted_Connection=True;");
         private SqlCommandBuilder cmdb;
         public DataSet ds = new DataSet();
         public SqlDataAdapter da;
         public SqlCommand cmd;
 
-        public bool connect()//Connect to Database
-        {
-            try
-            {
+        public bool connect(){//Connect to Database
+            try {
                 con.Open();
                 //msn = connection succesfuly
                 return true;
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 //msn = connection fail.
                 return false;
-            }
-            finally
-            {
-                con.Close();
+            } finally {
+               con.Close();
             }
         }
 
-        public void Query(string sql, string table) 
-        {
+        public void Query(string sql, string table) {
             ds.Tables.Clear();
             da = new SqlDataAdapter(sql, con);
             cmdb = new SqlCommandBuilder(da);
@@ -48,18 +40,14 @@ namespace Appname // Here you have to put the application name.
              */
         }
 
-        public bool Insert(string sql)
-        {
+        public bool Insert(string sql) {
             con.Open();
             cmd = new SqlCommand(sql, con);
             int i = cmd.ExecuteNonQuery();
             con.Close();
-            if (i > 0)
-            {
+            if (i > 0) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
             /*inser data from frm control
@@ -69,19 +57,15 @@ namespace Appname // Here you have to put the application name.
              */
         }
 
-        public bool Delete(string table, string condition)
-        {
+        public bool Delete(string table, string condition) {
             con.Open();
             string sql = "DELETE FROM " + table + " WHERE " + condition;
             cmd = new SqlCommand(sql, con);
             int i = cmd.ExecuteNonQuery();
             con.Close();
-            if (i > 0)
-            {
+            if (i > 0) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
             /*
@@ -96,19 +80,15 @@ namespace Appname // Here you have to put the application name.
          * txtcost.text = dgv.Cell[2].value.toString();
          */
 
-        public bool Update(string table, string column, string condition)
-        {
+        public bool Update(string table, string column, string condition) {
             con.Open();
             string sql = "UPDATE " + table + " SET " + column + " WHERE " + condition;
             cmd = new SqlCommand(sql, con);
             int i = cmd.ExecuteNonQuery();
             con.Close();
-            if (i > 0)
-            {
+            if (i > 0) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
 
